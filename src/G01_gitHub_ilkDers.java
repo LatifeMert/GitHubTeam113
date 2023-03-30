01743435f1db547e7da1959ff7cf14f93918018b
LatifeMert <latifemert.01@gmail.com>
2023-03-31 01:15:45 +0300

    IkinciGun2

--- src/G01_gitHub_ilkDers.java
+++ src/G01_gitHub_ilkDers.java
@@ -1,10 +1,11 @@
-public class gitHub_ilkDers {
+public class G01_gitHub_ilkDers {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("==============");
         System.out.println("have a wise day");
 
         System.out.println("gitadd-commit-push");
+        System.out.println("merhaba ben wise");
     }
 }
 
--- src/G02_gitHub_ilkDers.java
+++ src/G02_gitHub_ilkDers.java
@@ -1,3 +1,6 @@
 public class G02_gitHub_ilkDers {
+    public static void main(String[] args) {
+        System.out.println("Merhaba gitHub ben geldim.");
+    }
 
 }
